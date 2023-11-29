dd45bb5bc51f8cdfbcb2849d80a518b7cf57e8e0
Aaron Clayton <aaron.claytonj@outlook.com>
2023-11-29 14:32:12 -0600

    improved game loop

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 require './lib/game_logic'
 
+require 'pry-byebug'
+
 class Game
   
   def initialize()
@@ -7,14 +9,37 @@ class Game
   end
 
   def play()
+    # binding.pry
     until @game_logic.is_game_over
-      puts 'please enter something (q) to quit'
-      player_input = gets.chomp.downcase
-      if player_input.include? ('q')
-        break
-      end
+      # @game_logic.print_current_instructions
+      # player_input = gets.chomp.downcase
+      # if player_input.include? ('q')
+      #   break
+      # end
+      player_turns()
+
     end
 
     puts "thanks for playign"
+  end
+
+  def player_turns()
+    #show options
+    # select peice or quit
+    # select square to move to
+    # swap players
+    puts "#{@game_logic.current_mover.color}s turn"
+    puts "please select a piece (a1, a2.. a8, etc...) or (q) to quit game"
+    player_input = gets.chomp
+    if player_input[0] == 'q'
+      quit_game()
+    end
+
+    @game_logic.change_turns()
+  end
+
+  def quit_game()
+    puts "THANK YOU FOR PLAYING"
+    exit(true)
   end
 end
