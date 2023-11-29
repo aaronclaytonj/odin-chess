370404303ed850f3e5cb4f98503566e81e438c99
Aaron Clayton <aaron.claytonj@outlook.com>
2023-11-29 14:33:15 -0600

    added change_turns, print_current_instructions, and added some attributes

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,7 +1,38 @@
+require './lib/game_state'
+
 class GameLogic
 
+  INSTRUCTIONS = 'Please enter valid  (q) to quit'
+  SELECT_PIECE = 'Select a square (a1,a2,etc..)'
+
+  attr_reader :current_mover
+  
+  def initialize()
+    @white = GameState.new
+    @black = GameState.new
+    @current_mover = @white
+    @white.color = :white
+    @black.color = :black
+  end
 
   def is_game_over()
+    # puts "INFO() -> is_game_over() always return false here"
     false
+  end
+
+  def print_current_instructions()
+    if @current_mover.piece_picked
+      puts 'idk yet'
+    else
+      puts SELECT_PIECE
+    end
+  end
+  
+  def change_turns()
+    if @current_mover == @white
+      @current_mover = @black
+    else
+      @current_mover = @white
+    end
   end
 end
